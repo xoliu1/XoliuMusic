7e72b96f0e3059f9cff2581b284f043809114600
xoliu1 <xoliu6@gmail.com>
2023-10-26 21:11:57 +0800

    更新我的界面

--- app/src/main/java/com/xoliu/xoliumusic/ui/Main/Fragment/MineFragment.kt
+++ app/src/main/java/com/xoliu/xoliumusic/ui/Main/Fragment/MineFragment.kt
@@ -18,34 +18,19 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MineFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_mine, container, false)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MineFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance() =
             MineFragment().apply {
--- app/src/main/java/com/xoliu/xoliumusic/ui/Main/MainActivity.kt
+++ app/src/main/java/com/xoliu/xoliumusic/ui/Main/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.xoliu.xoliumusic.ui.Main
 
-import android.os.Build
+
+
 import android.os.Bundle
 import android.text.InputType
+import android.util.Log
 import android.view.Menu
+import android.view.View
 import android.view.inputmethod.EditorInfo
-import androidx.annotation.RequiresApi
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.MenuItemCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -19,6 +23,7 @@ import com.xoliu.xoliumusic.ui.tools.shiqv
 
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
+    private val TAG = "xoliu2"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -37,15 +42,15 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
      * @author xoliu
      * @create 23-10-23
      **/
-    @RequiresApi(Build.VERSION_CODES.N_MR1)
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(com.xoliu.xoliumusic.R.menu.menu_search, menu)
-        //定义出searchView实体
-        var searchItem = menu?.findItem(com.xoliu.xoliumusic.R.id.action_search)
-        var searchView = MenuItemCompat.getActionView(searchItem) as SearchView
 
-        //初始化SearchView
-        initSearchView(searchView)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+        //定义出searchView实体
+
+
+
+
+
 
 
 
@@ -62,39 +67,39 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
 //        val navController = findNavController(this, com.xoliu.xoliumusic.R.id.nav_host_fragment)
 //        setupWithNavController(binding.btmnv, navController)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        binding.btmnv.setupWithNavController(navHostFragment.navController)
+         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+         binding.btmnv.setupWithNavController(navHostFragment.navController)
 //        val navController = findNavController(R.id.nav_host_fragment)
 //        binding.btmnv.setupWithNavController(navController)
     }
 
-    @RequiresApi(Build.VERSION_CODES.N_MR1)
+
     fun initSearchView(searchView: SearchView){
         //图标是否显示在搜索框内
         searchView.setIconifiedByDefault(true)
 
         //是否显示提交按钮
         searchView.isSubmitButtonEnabled = true
-
         //把回车键设置成搜索
         searchView.setImeOptions(EditorInfo.IME_ACTION_SEARCH)
-
         //获取焦点
         //searchView.focusable = View.FOCUSABLE
         searchView.requestFocusFromTouch()
-
+        Log.d(TAG, "initSearchView: 1")
         //提示词
-        searchView.revealOnFocusHint = true
-        searchView.queryHint = "薛之谦"
-
+       // searchView.revealOnFocusHint = true
+        searchView.queryHint = "sdfdsfsd"
+        Log.d(TAG, "initSearchView: 1")
         //设置输入字符
         searchView.inputType = InputType.TYPE_CLASS_NUMBER
+        Log.d(TAG, "initSearchView: 1")
     }
 
     fun initToolbar(toolbar: Toolbar){
         toolbar.setNavigationOnClickListener {
             binding.drawerLayout.open()
         }
+
         toolbar.setOnMenuItemClickListener { menuItem ->
 
             when (menuItem.itemId) {
